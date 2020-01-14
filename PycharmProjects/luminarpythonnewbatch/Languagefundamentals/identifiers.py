6d43c61e51f608676b54ef03afbfc7f02f04f531
celiashebin <celiamariajames@gmail.com>
2020-01-14 10:47:32 +0530

    first commit

---
+++ PycharmProjects/luminarpythonnewbatch/Languagefundamentals/identifiers.py
@@ -0,0 +1,9 @@
+no=2
+print("no=",no)
+
+print(no,"=no")
+
+print("The value 10 is in no")
+
+
+print("no=",no)
---
+++ PycharmProjects/luminarpythonnewbatch/Languagefundamentals/swap.py
@@ -0,0 +1,8 @@
+no1=10
+no2=20
+temp=no1
+no1=no2
+no2=temp
+print("no1=",no1)
+print("no2=",no2)
+
